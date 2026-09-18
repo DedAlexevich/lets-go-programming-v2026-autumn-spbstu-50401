fe7ad8f9e778bb7ddecd08bc17bbb5795b65c590
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-09-18 10:51:36 +0300

    [TASK-1] Complete

--- petr.kuznetsov/task-1/cmd/calculator/main.go
+++ petr.kuznetsov/task-1/cmd/calculator/main.go
@@ -3,6 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Initial commit")
+	var a, b int
+	var op rune
+	_, efirst := fmt.Scan(&a)
+	_, esecond := fmt.Scan(&b)
+	fmt.Scanf("%c\n", &op)
+
+	if efirst != nil {
+		fmt.Println("Invalid first operand")
+		return
+	} else if esecond != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	switch op {
+	case '+':
+		fmt.Println(a + b)
+	case '-':
+		fmt.Println(a - b)
+	case '*':
+		fmt.Println(a * b)
+	case '/':
+		if b == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(a / b)
+		}
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
 }
 
